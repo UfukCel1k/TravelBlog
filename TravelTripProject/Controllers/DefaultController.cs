a00e72494cb30759c6829554f0f1f327b6bc63b5
Ufuk Çelik <ufukcelik.dev@gmail.com>
2023-05-08 01:35:26 +0300

    TravelBlog

--- TravelTripProject/Controllers/DefaultController.cs
+++ TravelTripProject/Controllers/DefaultController.cs
@@ -12,7 +12,7 @@ namespace TravelTripProject.Controllers
         Context c = new Context();
         public ActionResult Index()
         {
-            var values = c.Blogs.ToList();
+            var values = c.Blogs.Take(4).ToList();
             return View(values);
         }
         public ActionResult About()
@@ -28,6 +28,21 @@ namespace TravelTripProject.Controllers
         public PartialViewResult partial2()
         {
             var value = c.Blogs.Where(x => x.ID == 1).ToList();
+            return PartialView(value);
+        }
+        public PartialViewResult partial3()
+        {
+            var value = c.Blogs.ToList();
+            return PartialView(value);
+        }
+        public PartialViewResult partial4()
+        {
+            var value = c.Blogs.Take(3).ToList();
+            return PartialView(value);
+        }
+        public PartialViewResult partial5()
+        {
+            var value = c.Blogs.Take(3).OrderByDescending(x => x.ID).ToList();
             return PartialView(value);
         }
     }
