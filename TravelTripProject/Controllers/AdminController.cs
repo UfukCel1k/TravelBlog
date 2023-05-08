b0f5357d146e3eb15391bb457ccb6416fa16d5d7
Ufuk Çelik <ufukcelik.dev@gmail.com>
2023-05-09 00:55:56 +0300

    TravelBlog

---
+++ TravelTripProject/Controllers/AdminController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProject.Models.Classes;
+
+namespace TravelTripProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var value = c.Blogs.ToList();
+            return View(value);
+        }
+        [HttpGet]
+        public ActionResult NewBlog()
+        {
+            return View();
+        }   
+        [HttpPost]
+        public ActionResult NewBlog(Blog P)
+        {
+            c.Blogs.Add(P);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteBlog(int id)
+        {
+            var b = c.Blogs.Find(id);
+            c.Blogs.Remove(b);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult GetBlog(int id)
+        {
+            var b1 = c.Blogs.Find(id);
+            return RedirectToAction("GetBlog", b1);
+        }
+        public ActionResult UpdateBlog(Blog b)
+        {
+            var blg = c.Blogs.Find(b.ID);
+            blg.Description = b.Description;
+            blg.Heading = b.Heading;
+            blg.BlogImange = b.BlogImange;
+            blg.Date = b.Date;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
